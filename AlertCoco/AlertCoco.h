615160f7c473f1694f19708543394edf51313281
Gesdes <50413650+Gesdes@users.noreply.github.com>
2021-11-29 21:51:28 -0600

    configuracion

---
+++ AlertCoco/AlertCoco.h
@@ -0,0 +1,18 @@
+//
+//  AlertCoco.h
+//  AlertCoco
+//
+//  Created by Oscar San juan on 29/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlertCoco.
+FOUNDATION_EXPORT double AlertCocoVersionNumber;
+
+//! Project version string for AlertCoco.
+FOUNDATION_EXPORT const unsigned char AlertCocoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertCoco/PublicHeader.h>
+
+
